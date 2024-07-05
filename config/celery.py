36fefd4f28443e1b1d966cfa895dcc0245870e9e
Aristov Sergei <maxmit83@gmail.com>
2024-07-05 20:56:44 +0500

    Исправление дублирования настройки Celery

--- config/celery.py
+++ config/celery.py
@@ -8,9 +8,3 @@ app = Celery('config')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
-
-# Настройка параметров брокера
-app.conf.update(
-    broker_url='redis://localhost:6379/0',  # URL для подключения к брокеру
-    broker_connection_retry_on_startup=True,  # Повторное подключение к брокеру
-)
